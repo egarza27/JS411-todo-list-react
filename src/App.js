97d2fa3eed766570c34fcd74f68096564a227c4a
egarza27 <elizabethigarza10@gmail.com>
2023-05-09 16:59:35 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Liz</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isClicked: false,
+      inputValue: "",
+      listOfTodos: [],
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      listOfTodos: [...this.state.listOfTodos, this.state.inputValue],
+    });
+    this.setState({ inputValue: "" });
+  };
+
+  handleClick = () => {
+    this.state.isClicked
+      ? this.setState({
+          isClicked: false,
+        })
+      : this.setState({ isClicked: true });
+  };
+
+  handleDelete = (idx) => {
+    console.log(this.state);
+    this.setState({
+      listOfTodos: this.state.listOfTodos.filter((todo, i) => i !== idx),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header
+          style={{
+            marginTop: "150px",
+            marginBottom: "0px",
+            fontWeight: "bold",
+            fontSize: "30px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          List Of Todos
+        </header>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        ></input>
+        <button
+          style={{ marginTop: "100px" }}
+          type="button"
+          onClick={this.handleSubmit}
+        >
+          Add Todo
+        </button>
+        <ol>
+          {this.state.listOfTodos.map((todo, idx) => {
+            return (
+              <li key={idx}>
+                {todo}
+                {"  "}
+                <button type="button" onClick={() => this.handleDelete(idx)}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
